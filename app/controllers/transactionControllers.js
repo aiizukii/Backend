14c182faa677c7c578aace548c65c21856c3ad73
teukudr <teukudr@gmail..com>
2024-03-03 23:35:49 +0700

    update checkout

--- app/controllers/transactionControllers.js
+++ app/controllers/transactionControllers.js
@@ -68,6 +68,26 @@ module.exports = {
     const totalOngkir = checkout.hargaOngkir;
     const totalPrice = (productPrice * totalBarang) + totalOngkir;
 
+            // Memasukkan data produk ke dalam array
+            const productsArray = [];
+            checkout.Product.forEach(product => {
+              productsArray.push({
+                id: product.id,
+                kodebarang: product.kodebarang,
+                namabarang: product.namabarang,
+                image: product.image,
+                image2: product.image2,
+                image3: product.image3,
+                typebarang: product.typebarang,
+                deskripsibarang: product.deskripsibarang,
+                stockbarang: product.stockbarang,
+                satuanbarang: product.satuanbarang,
+                price: product.price,
+                createdAt: product.createdAt,
+                updatedAt: product.updatedAt
+              });
+            });
+
     return {
       id: checkout.id,
       usersId: checkout.usersId,
